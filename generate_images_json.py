ef5f7dcfe21c7f32047d2da830dc54a48f49a588
saltedfishjun <130265856+saltedfishjun@users.noreply.github.com>
2024-07-23 23:28:08 +0800

    Update generate_images_json.py

--- generate_images_json.py
+++ generate_images_json.py
@@ -3,5 +3,10 @@ import json
 
 image_folder = 'images'
 image_list = [f for f in os.listdir(image_folder) if os.path.isfile(os.path.join(image_folder, f))]
+
+print(f"Found images: {image_list}")  # Debug line
+
 with open('images.json', 'w') as f:
     json.dump(image_list, f, indent=4)
+
+print("images.json has been written")  # Debug line
